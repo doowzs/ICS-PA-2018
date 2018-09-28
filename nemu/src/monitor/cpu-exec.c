847294c87e3cb33d898dad68c705e979f7ac4df4
tracer-ics2018 <tracer@njuics.org>
2018-09-28 23:07:36 +0800

    >  compile 171860508 doowzs Linux DESKTOP-OT4H596 4.4.0-17134-Microsoft #285-Microsoft Thu Aug 30 17:31:00 PST 2018 x86_64 GNU/Linux  23:07:36 up 6 days, 0 min,  0 users,  load average: 0.52, 0.58, 0.59 ed14d442624b2c2e8805e6e661ddb36912559918

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -60,7 +60,7 @@ void cpu_exec(uint64_t n) {
         printflog("[\033[1;36mCPU\033[0m] \33[1;31mABORT\33[0m at eip = 0x%08x\n\n", cpu.eip);
       } 
 			else if (nemu_state == NEMU_STOP) {
-				printflog("[\033[1;36mCPU\033[0m] Stopped due to \33[1;31mWP\33[0m change.\n");
+				printflog("[\033[1;36mCPU\033[0m] \33[1;31mWP CHANGED\33[0m at eip = 0x%08x\n\n", cpu.eip);
 			}
 			return;
     }
