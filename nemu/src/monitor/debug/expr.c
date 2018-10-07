3eeabb81248a002f015c527b3d7646275ab5236f
tracer-ics2018 <tracer@njuics.org>
2018-10-07 19:47:39 +0800

    >  compile 171860508 doowzs Linux pixelbook 4.4.141-14563-g78fd9f781aae #1 SMP PREEMPT Thu Sep 13 00:00:24 PDT 2018 x86_64 x86_64 x86_64 GNU/Linux  19:47:39 up 15 days,  8:13,  0 users,  load average: 0.50, 0.39, 0.41 f066ff9d99b0eddf367e0681d4249cf3c22dcb9

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -290,7 +290,7 @@ static int read_reg(int pos, bool *success) {
 	} else if (strcmp(name, "eflags") == 0) {
 		int eflags32 = cpu.eflags32;
 		for (int i = 0; i < 32; ++i) printf("%1d", cpu.gpe[i]);
-		printf("\n");
+		printf("\n %ld \n", sizeof(bool));
 		for (int i = 0; i < 32; ++i) {
 			printf("%1d", (eflags32 & 1) ? 1 : 0);
 			eflags32 >>= 1;
