105135c10af68f45c1520a1fa36b7ed4018084ae
tracer-ics2018 <tracer@njuics.org>
2018-10-09 17:41:34 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.141-14563-g78fd9f781aae #1 SMP PREEMPT Thu Sep 13 00:00:24 PDT 2018 x86_64 x86_64 x86_64 GNU/Linux  17:41:34 up  7:44,  0 users,  load average: 1.43, 0.68, 0.61 f72501b9bf7bfb3ae079432099de0d1eecfd2514

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -155,7 +155,8 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+	assert (width >= 1 && width <= 4);
+	*dest = (*src1 << ((4 - width) << 3)) >> ((4 - width) << 3);
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
