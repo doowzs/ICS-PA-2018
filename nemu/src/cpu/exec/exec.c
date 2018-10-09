e074d0a56305a37f0b2278eb468c39bbac1ffc70
tracer-ics2018 <tracer@njuics.org>
2018-10-09 11:41:35 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.141-14563-g78fd9f781aae #1 SMP PREEMPT Thu Sep 13 00:00:24 PDT 2018 x86_64 x86_64 x86_64 GNU/Linux  11:41:35 up  1:44,  0 users,  load average: 0.69, 0.54, 0.51 5381f600ee3ccc798f752495a1ce76b967cd5076

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -185,7 +185,7 @@ opcode_entry opcode_table [512] = {
   /* 0xac */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xb0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xb4 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xb8 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0xb8 */	EMPTY, EMPTY, IDEXW(E2G, movzx, 1), IDEX(E2G, movzx),
   /* 0xbc */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc4 */	EMPTY, EMPTY, EMPTY, EMPTY,
