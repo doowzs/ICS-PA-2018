d86cc43832dd1098858aea05d329c9107440c225
tracer-ics2018 <tracer@njuics.org>
2018-10-11 16:31:43 +0800

    >  run 171860508 doowzs Linux localhost 4.4.153-15148-g02f4669441aa #1 SMP PREEMPT Tue Oct 2 00:32:59 PDT 2018 x86_64 GNU/Linux  16:31:43 up  1:54,  0 users,  load average: 1.08, 0.80, 0.83 5fab71bc913f0d02ca3fdac2de062c28d351143c

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -60,7 +60,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 
 void* memset(void* v, int c, size_t n) {
   int8_t c8 = (int8_t) c;
-  int32_t c32 = c8 + (c8 << 8) + (c8 << 16) + (c8 << 24);
+  int32_t c32 = (int32_t) c8 + ((int32_t) c8 << 8) + ((int32_t) c8 << 16) + ((int32_t) c8 << 24);
 
   int8_t *pv = (int8_t *) v;
 
