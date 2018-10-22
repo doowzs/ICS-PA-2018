266155fba6a01a2df971e5f37dcc6c303dc5e8ea
tracer-ics2018 <tracer@njuics.org>
2018-10-22 08:50:40 +0800

    >  run 171860508 doowzs1999 Linux penguin 4.14.67-07156-gc116f2c8c400 #1 SMP PREEMPT Sun Sep 9 14:28:13 PDT 2018 x86_64 GNU/Linux  08:50:40 up 1 day, 11:23,  2 users,  load average: 0.03, 0.11, 0.16 3d8a407b118d3582c7d553eb54f9e103d4bf7cd9

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -4,15 +4,16 @@
 
 const int KBD_REG_ADDR = 0x60;
 
+int next_keycode = 0;
+
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = inl(KBD_REG_ADDR);
-      if (kbd->keycode != (_KEY_NONE | 0x8000)) { 
-        kbd->keydown = 1;
-      }
+      
+      int next_keycode = inl(KBD_REG_ADDR);
+      kbd->keydown = (next_keycode == (kbd->keycode | 0x8000) ? 0 : 1);
+      kbd->keycode = next_keycode;
       return sizeof(_KbdReg);
     }
   }
