fdc4b123a8f4087db79ae642084e9807dc1b2a63
tracer-ics2018 <tracer@njuics.org>
2018-10-22 17:38:24 +0800

    >  compile 171860508 doowzs1999 Linux penguin 4.14.67-07156-gc116f2c8c400 #1 SMP PREEMPT Sun Sep 9 14:28:13 PDT 2018 x86_64 GNU/Linux  17:38:24 up  1:27,  6 users,  load average: 0.20, 0.09, 0.06 bffb1108a350ffc64b4d1ac496a3a4bff0a2c48e

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,14 +3,18 @@
 #include <amdev.h>
 #include <klib.h>
 
+#define SCREEN_PORT 0x100
+#define SCREEN_H 300
+#define SCREEN_W 400
+
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 0;
-      info->height = 0;
+      info->width = SCREEN_W;
+      info->height = SCREEN_H;
       return sizeof(_VideoInfoReg);
     }
   }
@@ -21,6 +25,11 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
+
+      /* test code */
+      int i;
+      int size = screen_width() * screen_height();
+      for (i = 0; i < size; i ++) fb[i] = i;
 
       if (ctl->sync) {
         // do nothing, hardware syncs.
