7bdc2dc2fe3e8fe2e2e303d6cde2dbfb03644da9
tracer-ics2018 <tracer@njuics.org>
2018-11-02 14:39:26 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  14:39:26 up 13 min,  0 users,  load average: 1.10, 1.37, 1.00 6f801966b1cb84c7eae855aa66e37e765baeecb3

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -41,54 +41,42 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
-  rtl_sext(&t1, &id_dest->val, id_dest->width);
-  if (id_src->val == 1) {
-    rtl_li(&t2, 0);
-    rtl_set_OF(&t2);
-  }
-  rtl_subi(&t0, &id_src->val, 1);
-	rtl_sar(&t1, &id_dest->val, &t0);
-  rtl_andi(&t2, &t1, 1);
-  rtl_set_CF(&t2);
+#if defined(DIFF_TEST)
+  void difftest_skip_flg();
+  difftest_skip_flg();
+#endif
 
-  rtl_sari(&t1, &t1, 1);
-  rtl_update_ZFSFPF(&t1, id_dest->width);
-	operand_write(id_dest, &t1);
+  rtl_sext(&t2, &id_dest->val, id_dest->width);
+	rtl_sar(&at, &t2, &id_src->val);
+	operand_write(id_dest, &at);
+  // unnecessary to update CF and OF in NEMU
+  rtl_update_ZFSFPF(&t2, id_dest->width);
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
-  rtl_subi(&t0, &id_src->val, 1);
-	rtl_shl(&t1, &id_dest->val, &t0);
-  rtl_msb(&t2, &t1, id_dest->width);
-  rtl_set_CF(&t2);
+#if defined(DIFF_TEST)
+  void difftest_skip_flg();
+  difftest_skip_flg();
+#endif
 
-  printf("(%10x) ", t1);
-
-  rtl_shli(&t1, &t1, 1);
-  rtl_update_ZFSFPF(&t1, id_dest->width);
-	operand_write(id_dest, &t1);
-  printf("%10x %10x %10x\n", id_dest->val, id_src->val, t1);
-
-  rtl_msb(&t3, &t1, id_dest->width);
-  rtl_xor(&t2, &t2, &t3);
-  rtl_set_OF(&t2);
+	rtl_shl(&t2, &id_dest->val, &id_src->val);
+	operand_write(id_dest, &t2);
+  // unnecessary to update CF and OF in NEMU
+  rtl_update_ZFSFPF(&t2, id_dest->width);
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
-  if (id_src->val == 1) {
-    rtl_msb(&t2, &id_dest->val, id_dest->width);
-    rtl_set_OF(&t2);
-  }
-  rtl_subi(&t0, &id_src->val, 1);
-	rtl_shr(&t1, &id_dest->val, &t0);
-  rtl_andi(&t2, &t1, 1);
-  rtl_set_CF(&t2);
+#if defined(DIFF_TEST)
+  void difftest_skip_flg();
+  difftest_skip_flg();
+#endif
 
-  rtl_shri(&t1, &t1, 1);
-  rtl_update_ZFSFPF(&t1, id_dest->width);
-	operand_write(id_dest, &t1);
+	rtl_shr(&t2, &id_dest->val, &id_src->val);
+	operand_write(id_dest, &t2);
+  // unnecessary to update CF and OF in NEMU
+  rtl_update_ZFSFPF(&t2, id_dest->width);
   print_asm_template2(shr);
 }
 
