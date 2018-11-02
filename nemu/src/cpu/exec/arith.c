18ebd5060451dfc4acc2329babb4c8a350b29b42
tracer-ics2018 <tracer@njuics.org>
2018-11-02 15:05:34 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  15:05:34 up 39 min,  0 users,  load average: 1.69, 1.92, 1.69 2c6809d78227f3eac09802c2a88527f6221c5550

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -138,16 +138,20 @@ make_EHelper(adc) {
 }
 
 make_EHelper(sbb) {
-  rtl_sub(&t2, &id_dest->val, &id_src->val);
-  rtl_setrelop(RELOP_LTU, &t3, &id_dest->val, &t2);
-  rtl_get_CF(&t1);
-  rtl_sub(&t2, &t2, &t1);
-  operand_write(id_dest, &t2);
+  rtl_sub(&t1, &id_dest->val, &id_src->val);
+  rtl_setrelop(RELOP_LTU, &t0, &id_src->val, &t1);
+  rtl_setrelop(RELOP_LTU, &t3, &id_dest->val, &t1);
+  rtl_or(&t3, &t0, &t3);
+  rtl_get_CF(&t0);
+  rtl_sub(&t2, &t1, &t0);
 
+  operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
 
-  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &t2);
-  rtl_or(&t0, &t3, &t0);
+  rtl_setrelop(RELOP_LTU, &t0, &t0, &t2);
+  rtl_setrelop(RELOP_LTU, &t1, &t1, &t2);
+  rtl_or(&t0, &t0, &t1);
+  rtl_or(&t0, &t0, &t3);
   rtl_set_CF(&t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
