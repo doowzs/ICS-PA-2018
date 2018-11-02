3357583fab8dd768c620f6d1429a0175d90aee06
tracer-ics2018 <tracer@njuics.org>
2018-11-02 15:18:01 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  15:18:01 up 52 min,  0 users,  load average: 2.78, 2.01, 1.73 346f8b28c3431db5825eaa1fa92ba3bc8b0eb43f

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -160,10 +160,6 @@ make_EHelper(sbb) {
 }
 
 make_EHelper(mul) {
-#if defined(DIFF_TEST)
-  void difftest_skip_flg();
-  difftest_skip_flg();
-#endif
   rtl_lr(&t0, R_EAX, id_dest->width);
   rtl_mul_lo(&t1, &id_dest->val, &t0);
 
@@ -189,10 +185,6 @@ make_EHelper(mul) {
 
 // imul with one operand
 make_EHelper(imul1) {
-#if defined(DIFF_TEST)
-  void difftest_skip_flg();
-  difftest_skip_flg();
-#endif
   rtl_lr(&t0, R_EAX, id_dest->width);
   rtl_imul_lo(&t1, &id_dest->val, &t0);
 
@@ -218,10 +210,6 @@ make_EHelper(imul1) {
 
 // imul with two operands
 make_EHelper(imul2) {
-#if defined(DIFF_TEST)
-  void difftest_skip_flg();
-  difftest_skip_flg();
-#endif
   rtl_sext(&t0, &id_src->val, id_dest->width);
   rtl_sext(&t1, &id_dest->val, id_dest->width);
 
@@ -233,10 +221,6 @@ make_EHelper(imul2) {
 
 // imul with three operands
 make_EHelper(imul3) {
-#if defined(DIFF_TEST)
-  void difftest_skip_flg();
-  difftest_skip_flg();
-#endif
   rtl_sext(&t0, &id_src->val, id_dest->width);
   rtl_sext(&t1, &id_src2->val, id_dest->width);
   rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
@@ -248,10 +232,6 @@ make_EHelper(imul3) {
 }
 
 make_EHelper(div) {
-#if defined(DIFF_TEST)
-  void difftest_skip_flg();
-  difftest_skip_flg();
-#endif
   switch (id_dest->width) {
     case 1:
       rtl_lr(&t0, R_AX, 2);
@@ -283,10 +263,6 @@ make_EHelper(div) {
 }
 
 make_EHelper(idiv) {
-#if defined(DIFF_TEST)
-  void difftest_skip_flg();
-  difftest_skip_flg();
-#endif
   switch (id_dest->width) {
     case 1:
       rtl_lr(&t0, R_AX, 2);
