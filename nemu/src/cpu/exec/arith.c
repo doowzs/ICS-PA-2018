ca0857e825d288f9f702e18ab55814479989afc7
tracer-ics2018 <tracer@njuics.org>
2018-11-02 15:14:37 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  15:14:37 up 48 min,  0 users,  load average: 1.63, 1.38, 1.51 53277b1239c2fe8de6ae7ae8df7524a8360bf7a0

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -144,7 +144,6 @@ make_EHelper(sbb) {
   rtl_sub(&t2, &t1, &t0);
 
   operand_write(id_dest, &t2);
-  printf("%10x-%10x->%10x\n", id_dest->val, id_src->val, t2);
   rtl_update_ZFSFPF(&t2, id_dest->width);
 
   rtl_setrelop(RELOP_LTU, &t0, &t1, &t0);
