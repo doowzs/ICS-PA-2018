cc4ebdfcf4d8fbe4985fc6a7de2717805530626a
tracer-ics2018 <tracer@njuics.org>
2018-11-02 14:58:06 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  14:58:06 up 32 min,  0 users,  load average: 2.52, 1.86, 1.49 9dee20ebb875230568685f1a6a52d07a69e4219

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -111,19 +111,21 @@ make_EHelper(neg) {
 }
 
 make_EHelper(adc) {
-  rtl_add(&t2, &id_dest->val, &id_src->val);
-  rtl_setrelop(RELOP_LTU, &t0, &t2, &id_src->val);
-  rtl_setrelop(RELOP_LTU, &t3, &t2, &id_dest->val);
+  rtl_add(&t1, &id_dest->val, &id_src->val);
+  rtl_setrelop(RELOP_LTU, &t0, &t1, &id_src->val);
+  rtl_setrelop(RELOP_LTU, &t3, &t1, &id_dest->val);
   rtl_or(&t3, &t0, &t3);
-  rtl_get_CF(&t1);
-  rtl_add(&t2, &t2, &t1);
+  rtl_get_CF(&t0);
+  rtl_add(&t2, &t1, &t0);
 
   operand_write(id_dest, &t2);
-  printf("%10x+%10x->%10x", id_dest->val, id_src->val, t2);
+  printf("%10x+%10x->%10x\n", id_dest->val, id_src->val, t2);
   rtl_update_ZFSF(&t2, id_dest->width);
 
+  rtl_setrelop(RELOP_LTU, &t1, &t1, &t0);
   rtl_setrelop(RELOP_LTU, &t0, &t2, &id_dest->val);
-  rtl_or(&t0, &t3, &t0);
+  rtl_or(&t0, &t0, &t1);
+  rtl_or(&t0, &t0, &t3);
   rtl_set_CF(&t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
