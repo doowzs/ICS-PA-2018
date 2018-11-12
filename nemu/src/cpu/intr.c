c26538324fd271f3b41398b8ff4828cd5db38b6b
tracer-ics2018 <tracer@njuics.org>
2018-11-12 18:58:37 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  18:58:37 up  1:43,  0 users,  load average: 1.97, 2.15, 1.92 3370a1499d2031289e3dc54de0b8e2c9bbb6e8d8

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,11 +5,16 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  uint16_t offset_15_0 = 0, offset_31_16 = 0; 
-  offset_15_0 = vaddr_read(cpu.IDTR.base + (NO << 6), 2);
-  offset_31_16 = vaddr_read(cpu.IDTR.base + (NO << 6) + 48, 2);
-  printf("OFFSETs are: 0x%04x 0x%04x", offset_31_16, offset_15_0);
-  rtl_j(offset_15_0 | (offset_31_16 << 16));
+  GateDesc gd;
+  vaddr_t gd_addr = cpu.IDTR.base + NO * 0x10;
+  gd.val[0] = vaddr_read(gd_addr, 4);
+  gd.val[1] = vaddr_read(gd_addr + 4, 4);
+  printf("OFFSETs are: 0x%04x 0x%04x", gd.offset_31_16, gd.offset_15_0);
+  if (gd.present) {
+    rtl_j((gd.offset_31_16 << 16) | gd.offset_15_0);
+  } else {
+    panic("Gate is not present!\n");
+  }
 }
 
 void dev_raise_intr() {
