cf23ed092a8904597cc7a6ab2e2d9a73148f865a
tracer-ics2018 <tracer@njuics.org>
2018-11-12 16:54:54 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  16:54:54 up  5:47,  0 users,  load average: 2.02, 1.84, 1.79 f94ade4c6e9ac856d2e42017c603a7f4baf9ad1

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,15 +5,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  // FIXME FIXME FIXME what is the start address???
   GateDesc gd;
   gd.val = vaddr_read(cpu.IDTR.base + (NO << 4), 16);
-  if (gd.present) {
-    vaddr_t ptar = gd.offset_15_0 | (gd.offset_31_16 << 16);
-    cpu.eip = ptar;
-  } else {
-    panic("gate is not valid! check at /cpu/intr.c");
-  }
+  rtl_j(gd.offset_15_0 | (gd.offset_31_16 << 16));
 }
 
 void dev_raise_intr() {
