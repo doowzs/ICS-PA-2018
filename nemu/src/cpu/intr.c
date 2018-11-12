55358f97e875cf287e5abdf09ce3e854414fe6b1
tracer-ics2018 <tracer@njuics.org>
2018-11-12 17:00:46 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  17:00:46 up  5:53,  0 users,  load average: 1.22, 1.49, 1.65 799efd305203cceaf563104fd5d0b2b853e0adf1

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,6 +7,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
   GateDesc gd;
   gd.val = vaddr_read(cpu.IDTR.base + (NO << 4), 16);
+  printf("offset1 is %08x\n", gd.offset_15_0);
   rtl_j(gd.offset_15_0 | (gd.offset_31_16 << 16));
 }
 
