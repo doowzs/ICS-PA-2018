f37437b7109624ead205c0f3c1a1b255015253fb
tracer-ics2018 <tracer@njuics.org>
2018-11-12 17:05:32 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  17:05:32 up  5:58,  0 users,  load average: 2.01, 1.98, 1.83 45599679233bfd33359d1ff531b544f17e103e76

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,10 +5,10 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  GateDesc gd;
-  gd.val = vaddr_read(cpu.IDTR.base + (NO << 4), 16);
-  printf("offset1 is %08x\n", gd.offset_15_0);
-  rtl_j(gd.offset_15_0 | (gd.offset_31_16 << 16));
+  uint16_t offset_15_0 = 0, offset_31_16 = 0; 
+  offset_15_0 = vaddr_read(cpu.IDTR.base + (NO << 6), 2);
+  printf("offset1 is %04x\n", offset_15_0);
+  rtl_j(offset_15_0 | (offset_31_16 << 16));
 }
 
 void dev_raise_intr() {
