ce2cbeb80d1ea7c5916458d6afabaefd669dc08a
tracer-ics2018 <tracer@njuics.org>
2018-11-14 14:42:48 +0800

    >  gdb 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  14:42:48 up 1 day, 21:28,  0 users,  load average: 1.20, 1.24, 1.26 298b90c8744f36cc8070f966596b0b64b986a4d

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -9,7 +9,7 @@ void vecnull();
 
 void print_tf(_Context *tf) {
   printf("_CONTEXT ADDR AT %p\n", tf);
-  //printf("ESI:    0x%08x\n", tf->esi);
+  printf("ESI:    0x%08x\n", tf->esi);
   printf("EBP:    0x%08x\n", tf->ebp);
   printf("EDI:    0x%08x\n", tf->edi);
   printf("EAX:    0x%08x\n", tf->eax);
