3003a711fc21997a821ff99c6ccc2e2192a14051
171860508-Tianyun Zhang <doowzs@qq.com>
2018-11-14 15:21:18 +0800

    [ADD] handle with 0x81 yield event
    Why: To make nanos handle with 0x81-yield interrupt event.
    
    How: If irq==0x81, assign _EVENT_YIELD to event type and make do_event
    function handle with this kind of event.
    
    Tags: event interrupt do_event irq_handler

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD:
-      printf("This is do_event in nanos/src/irq.c.\n");
+      printf("[\033[1;31mITR\033[0m]This is do_event in nanos/src/irq.c.\n");
       printf("You are seeing this message because a NO.0x81 yield event occurred.\n");
       break;
     default: panic("Unhandled event ID = %d", e.event);
