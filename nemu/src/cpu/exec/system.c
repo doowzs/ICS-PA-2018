2ca1cf54d250af123683611a4ff9ca771547fe72
tracer-ics2018 <tracer@njuics.org>
2018-11-14 15:01:46 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  15:01:46 up 1 day, 21:47,  0 users,  load average: 1.81, 1.65, 1.51 9d825e77998f003f7d0fe7f62f37f6d826a8b051

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -32,8 +32,9 @@ make_EHelper(mov_cr2r) {
 make_EHelper(int) {
   rtl_push(&cpu.eflags32);
   rtl_push(&cpu.CS);
-  rtl_push(&cpu.eip);
-  raise_intr(id_dest->val, decoding.seq_eip);
+  rtl_push(&decoding.seq_eip);
+  //FIXME FIXME FIXME
+  raise_intr(id_dest->val, 0x0);
 
   print_asm("int %s", id_dest->str);
 
