8f3ff22b43a2e08cd3bff248d509ad66709b85ff
tracer-ics2018 <tracer@njuics.org>
2018-11-14 15:28:07 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.153-15155-g656ec00c3ce2 #1 SMP PREEMPT Mon Oct 22 23:38:49 PDT 2018 x86_64 GNU/Linux  15:28:07 up 1 day, 22:13,  0 users,  load average: 2.29, 2.47, 2.10 8f087a732bdafb5ae00fdd469d3bf69d3270e9d

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -27,6 +27,8 @@ make_EHelper(pusha) {
       if (i == 4) {
         rtl_push(&t0);
       } else {
+        printf("16-bit pusha is buggy!\n");
+        TODO(); // BUGGY FIXME
         rtl_push((rtlreg_t *) &cpu.gpr[i]._16);
       }
     }
@@ -45,7 +47,25 @@ make_EHelper(pusha) {
 }
 
 make_EHelper(popa) {
-  TODO();
+  if (decoding.is_operand_size_16) {
+    for (int i = 7; i >= 0; --i) {
+      if (i == 4) {
+        rtl_pop(&t0);
+      } else {
+        printf("16-bit popa is buggy!\n");
+        TODO(); // BUGGY FIXME
+        rtl_pop((rtlreg_t *) &cpu.gpr[i]._16);
+      }
+    }
+  } else {
+    for (int i = 7; i >= 0; --i) {
+      if (i == 4) {
+        rtl_pop(&t0);
+      } else {
+        rtl_pop(&cpu.gpr[i]._32);
+      }
+    }
+  }
 
   print_asm("popa");
 }
