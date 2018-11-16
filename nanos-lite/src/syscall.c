8e0bd87204d7268f022176cada889944863401f4
tracer-ics2018 <tracer@njuics.org>
2018-11-16 18:45:46 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 GNU/Linux  18:45:46 up 1 day,  7:13,  0 users,  load average: 1.75, 1.65, 1.46 419d1a82929d5ecce3630a1d838850ae410bfb1e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,7 +7,12 @@ void syscall_ret(_Context *c, int val) {
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
+  char *pbuf;
+
   a[0] = c->GPR1;
+  a[1] = c->ebx;
+  a[2] = c->ecx;
+  a[3] = c->edx;
 
   switch (a[0]) {
     case SYS_exit:  // 0
@@ -16,6 +21,21 @@ _Context* do_syscall(_Context *c) {
     case SYS_yield: // 1
       _yield();
       syscall_ret(c, 0);
+      break;
+    case SYS_write: // 4
+      switch (a[1]) {
+        case 1:
+        case 2:
+          pbuf = (char *) a[2];
+          for (int i = 0; i < a[3]; ++i) {
+            _putc(*pbuf);
+            ++pbuf;
+          }
+          syscall_ret(c, a[3]);
+          break;
+        default:
+          panic("fd is not supported by SYS_write, fix in nanos/src/syscall.c");
+      }
       break;
     default: panic("Unhandled syscall ID = %d, fix in nanos/src/syscall.c", a[0]);
   }
