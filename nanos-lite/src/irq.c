fa9229fcc3e328032229fa689e14c070a5e9b1bc
tracer-ics2018 <tracer@njuics.org>
2018-11-16 17:59:19 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 GNU/Linux  17:59:19 up 1 day,  6:27,  0 users,  load average: 1.51, 1.49, 1.55 d2a4a2c4fac51211714cb0f8a5b2a80386b7741

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,8 +5,7 @@ _Context* do_syscall(_Context* c);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD:
-      printf("[\033[1;31mITR\033[0m]This is do_event in nanos/src/irq.c.\n");
-      printf("You are seeing this message because a NO.0x81 yield event occurred.\n");
+      Log("A yield event (0x81) is triggered.");
       break;
     case _EVENT_SYSCALL:
       do_syscall(c);
