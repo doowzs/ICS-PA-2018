84b950e26192c35f625277acba5b2f2d7eafd61a
tracer-ics2018 <tracer@njuics.org>
2018-11-16 17:19:20 +0800

    >  gdb 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 GNU/Linux  17:19:20 up 1 day,  5:47,  0 users,  load average: 1.37, 1.25, 1.38 91093703e559ce901486ae224fe7b6b6c1fe07d

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -8,7 +8,7 @@
 
 #if defined(__ISA_X86__)
 intptr_t _syscall_(int type, intptr_t a0, intptr_t a1, intptr_t a2){
-  int ret = -1; // WHY -1 ?
+  int ret = -1;
   asm volatile("int $0x80": "=a"(ret): "a"(type), "b"(a0), "c"(a1), "d"(a2));
   return ret;
 }
