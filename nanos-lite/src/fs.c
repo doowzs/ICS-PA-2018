95614a35730aa9b45aec6614edb2d02445a72428
tracer-ics2018 <tracer@njuics.org>
2018-11-20 14:49:41 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  14:49:41 up 20:11,  0 users,  load average: 0.71, 0.62, 0.64 c0878c4eb0b32663785aa553be547e707ce543

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -31,7 +31,7 @@ int fs_open(const char *pathname, int flags, int mode){
   // flags and mode are not used in nemu
   for (int i = 0; i < NR_FILES; ++i) {
     if (strcmp(pathname, file_table[i].name) == 0) {
-      file_table[i].open_offset = file_table[i].disk_offset;
+      file_table[i].open_offset = 0;
       return i;
     }
   }
@@ -61,7 +61,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
     panic("cannot read from STDOUT/STDERR. see nanos/src/fs.c");
    default:
     assert(fd > 2 && fd < NR_FILES);
-    file_table[fd].open_offset = file_table[fd].disk_offset + len;
+    file_table[fd].open_offset = len;
     return ramdisk_read(buf, file_table[fd].disk_offset, len);
   }
   return -1;
@@ -82,7 +82,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
       return len;
     default:
       assert(fd > 2 && fd < NR_FILES);
-      file_table[fd].open_offset = file_table[fd].disk_offset + len;
+      file_table[fd].open_offset = len;
       return ramdisk_write(buf, file_table[fd].disk_offset, len);
   }
   return -1;
@@ -95,10 +95,10 @@ size_t fs_lseek(int fd, size_t offset, int whence) {
       file_table[fd].open_offset = offset;
       break;
     case SEEK_CUR:
-      file_table[fd].open_offset = file_table[fd].disk_offset + offset;
+      file_table[fd].open_offset = file_table[fd].open_offset + offset;
       break;
     case SEEK_END:
-      file_table[fd].open_offset = file_table[fd].disk_offset + file_table[fd].size + offset;
+      file_table[fd].open_offset = file_table[fd].size + offset;
       break;
     default:
       panic("%d is not a valid whence! see nanos/src/fs.c", whence);
