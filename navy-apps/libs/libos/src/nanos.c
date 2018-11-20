7f2c8bce3bd837e09ffb84f442f055e62ba4fe44
tracer-ics2018 <tracer@njuics.org>
2018-11-20 11:37:45 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  11:37:45 up 16:59,  0 users,  load average: 1.26, 1.04, 0.80 c86b4f309f2a49656e74a5dd9b7f031ab9021e05

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -38,7 +38,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count){
-  assert(fd != 1);
+  assert(1 != 1);
   int ret = _syscall_(SYS_write, fd, (intptr_t) buf, count);
   return ret;
 }
