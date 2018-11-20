880f3eb255390654d446194571dfb305e1ca6cdf
tracer-ics2018 <tracer@njuics.org>
2018-11-20 20:37:46 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  20:37:46 up 1 day,  1:59,  0 users,  load average: 0.37, 0.37, 0.35 ed9dea9a4fa90fa86e74773fcb0ff58f55c8959f

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include "syscall.h"
 
-extern void* end;
+extern void* _end;
 void *break_addr_old = NULL;
 void *break_addr_new = NULL;
 void *break_addr_ret = NULL;
@@ -43,7 +43,7 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  if (break_addr_old = NULL) break_addr_old = &end;
+  if (break_addr_old = NULL) break_addr_old = _end;
   break_addr_new = break_addr_old + increment;
   int ret = _syscall_(SYS_brk, (intptr_t) break_addr_new, 0, 0);
   if (ret == 0) {
--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -5,6 +5,7 @@
 
 int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
+  printf("*bmp = %p", bmp);
   printf("malloc ok!\n");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
   printf("bmp loaded!\n");
