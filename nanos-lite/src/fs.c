5e6bc62069c73bcc336882dee6c44fd07cfd10d4
tracer-ics2018 <tracer@njuics.org>
2018-11-20 19:03:49 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  19:03:49 up 1 day, 25 min,  0 users,  load average: 0.62, 0.40, 0.43 c50f5b90af8faea212594f518b3ea261af81a44d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -18,6 +18,8 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
+  {"/dev/fb", 0, 0, 0, ramdisk_read, ramdisk_write},
+  {"/proc/dispinfo", 0, 0, 0, ramdisk_read, ramdisk_write},
 #include "files.h"
 };
 
