f0770ced447fc52fad3840eb49c96eabf1585ed6
tracer-ics2018 <tracer@njuics.org>
2018-11-20 15:21:07 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  15:21:07 up 20:42,  0 users,  load average: 0.64, 0.60, 0.63 f149a292d7f90478cf39a5469737c14c6e9a04cf

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -62,7 +62,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
    default:
     assert(fd > 2 && fd < NR_FILES);
     file_table[fd].open_offset += len;
-    return ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    return ramdisk_read(buf, file_table[fd].disk_offset, len);
   }
   return -1;
 }
