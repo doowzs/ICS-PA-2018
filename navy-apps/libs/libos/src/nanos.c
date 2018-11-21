e406183bac9cb51cdecb9711431bcc4cf3431969
tracer-ics2018 <tracer@njuics.org>
2018-11-21 15:12:28 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  15:12:28 up 1 day, 20:33,  0 users,  load average: 0.65, 0.82, 0.76 1f3ea47c37534115919ca72df26b556cc7748c36

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -4,6 +4,7 @@
 #include <sys/time.h>
 #include <assert.h>
 #include <time.h>
+#include <stdio.h>
 #include "syscall.h"
 
 #if defined(__ISA_X86__)
@@ -38,12 +39,14 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
+  char buf[128];
   void *br_old = (void *) _syscall_(SYS_brk, 0, 0, 0);
   void *br_new = (void *) _syscall_(SYS_brk, (uintptr_t) br_old + increment, 0, 0);
   if ((uintptr_t) br_new == (uintptr_t) br_old + increment) {
+    sprintf(buf, "returning %p\n", br_old);
+    _write(1, buf, 30);
     return br_old;
   } else {
-    assert(0);
     return (void *) -1;
   }
 }
