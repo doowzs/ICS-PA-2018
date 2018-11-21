07fa7ad1b52ad013756d36b913492ab484dec667
tracer-ics2018 <tracer@njuics.org>
2018-11-21 15:26:45 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  15:26:45 up 1 day, 20:48,  0 users,  load average: 0.99, 0.81, 0.82 cd5480dfeac42136c6ff6bbfc0c4f51ab16e39e3

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,6 +4,7 @@
 #include "syscall.h"
 
 extern void *_end;
+static void *brk = &(_end);
 
 void syscall_ret(_Context *c, int val) {
   c->GPRx = val;
@@ -147,9 +148,9 @@ _Context* do_syscall(_Context *c) {
      */
     case SYS_brk:
 #ifdef SYS_DEBUG
-      Log("SYS_brk(%p->%p)", _end, a[1]);
+      Log("SYS_brk(%p->%p)", brk, a[1]);
 #endif
-      _end = (void *) a[1];
+      brk = (void *) a[1];
       syscall_ret(c, 0);
       break;
 
