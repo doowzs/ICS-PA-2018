0658698e36d19a9353721489039e9d31d3f6c734
tracer-ics2018 <tracer@njuics.org>
2018-11-22 10:52:59 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  10:52:59 up 2 days, 16:14,  0 users,  load average: 0.96, 0.87, 0.87 ec95f73dc6dc6d3a544d6853dc1a51a4d14bdc9f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,7 +64,8 @@ size_t fs_read(int fd, void *buf, size_t len) {
   if (file_table[fd].open_offset >= file_table[fd].size) {
     return 0; // EOF
   } else {
-    if (file_table[fd].open_offset + len > file_table[fd].size) {
+    if (file_table[fd].size > 0
+        && file_table[fd].open_offset + len > file_table[fd].size) {
       len = file_table[fd].size - file_table[fd].open_offset;
     }
     size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
@@ -79,7 +80,8 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   if (file_table[fd].open_offset >= file_table[fd].size) {
     return 0; // Fail
   } else {
-    if (file_table[fd].open_offset + len > file_table[fd].size) {
+    if (file_table[fd].size > 0 
+        && file_table[fd].open_offset + len > file_table[fd].size) {
       len = file_table[fd].size - file_table[fd].open_offset;
     }
     size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
