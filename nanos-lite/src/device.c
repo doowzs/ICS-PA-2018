8de1d9408f9c7842cf5a165ad107d68e4cfe5700
tracer-ics2018 <tracer@njuics.org>
2018-11-22 11:36:44 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  11:36:44 up 2 days, 16:58,  0 users,  load average: 0.52, 0.67, 0.78 a41869df67e9dffb44079e33d90f6d28ae61eccc

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,7 +31,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   int w = len / sizeof(uint32_t), h = 1;
-  int x = offset % screen_width(), y = offset / screen_width();
+  int uoff = offset / sizeof(uint32_t);
+  int x = uoff % screen_width(), y = uoff / screen_width();
   Log("offset=%d, x=%d, y=%d, w=%d, h=%d\n", offset, x, y, w, h);
   draw_rect((uint32_t *) buf, x, y, w, h);
   return len;
