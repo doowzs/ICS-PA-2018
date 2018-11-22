95c1634749ae0ae3d0057aac7eb453af267f8748
tracer-ics2018 <tracer@njuics.org>
2018-11-22 10:43:43 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 x86_64 x86_64 GNU/Linux  10:43:43 up 2 days, 16:05,  0 users,  load average: 1.68, 1.15, 0.95 1756f5f84ef986b6aa9a0319a5af38e33de654c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,7 +26,7 @@ static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   strncpy((char *) buf, dispinfo, 128);
-  return len;
+  return 128;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
