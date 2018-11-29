923f43f146393904bdf0e488247bca44fc3ed531
tracer-ics2018 <tracer@njuics.org>
2018-11-29 14:53:37 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 GNU/Linux  14:53:37 up 5 days, 19:13,  0 users,  load average: 2.21, 2.11, 2.05 1247c025707d3bc6b8784e894ba1756a3fa1cf4

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -67,26 +67,28 @@ extern void difftest_attach() {
   void *handle;
   handle = dlopen(opened_ref_so_file, RTLD_LAZY | RTLD_DEEPBIND);
   assert(handle);
-
+Log("1");
   ref_difftest_memcpy_from_dut = dlsym(handle, "difftest_memcpy_from_dut");
   assert(ref_difftest_memcpy_from_dut);
-
+Log("2");
   ref_difftest_getregs = dlsym(handle, "difftest_getregs");
   assert(ref_difftest_getregs);
-
+Log("3");
   ref_difftest_setregs = dlsym(handle, "difftest_setregs");
   assert(ref_difftest_setregs);
-
+Log("4");
   ref_difftest_exec = dlsym(handle, "difftest_exec");
   assert(ref_difftest_exec);
-
+Log("5");
   void (*ref_difftest_init)(void) = dlsym(handle, "difftest_init");
   assert(ref_difftest_init);
-
+Log("6");
   Log("Differential testing is turned on.");
-
+Log("7");
   ref_difftest_init();
+Log("8");
   ref_difftest_memcpy_from_dut(ENTRY_START, guest_to_host(ENTRY_START), PMEM_SIZE);
+Log("9");
   ref_difftest_setregs(&cpu); 
 }
 
