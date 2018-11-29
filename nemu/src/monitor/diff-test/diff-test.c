7409be87ab80c22508ea4daaeb492b3b308eaa95
tracer-ics2018 <tracer@njuics.org>
2018-11-29 14:57:43 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15338-g1c38ea17cf5d #1 SMP PREEMPT Tue Nov 13 00:00:20 PST 2018 x86_64 GNU/Linux  14:57:43 up 5 days, 19:18,  0 users,  load average: 2.56, 2.27, 2.12 dd3a34754006322226c756056448de4b76d891c

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -67,23 +67,24 @@ extern void difftest_attach() {
   void *handle;
   handle = dlopen(opened_ref_so_file, RTLD_LAZY | RTLD_DEEPBIND);
   assert(handle);
-Log("1");
+
   ref_difftest_memcpy_from_dut = dlsym(handle, "difftest_memcpy_from_dut");
   assert(ref_difftest_memcpy_from_dut);
-Log("2");
+
   ref_difftest_getregs = dlsym(handle, "difftest_getregs");
   assert(ref_difftest_getregs);
-Log("3");
+
   ref_difftest_setregs = dlsym(handle, "difftest_setregs");
   assert(ref_difftest_setregs);
-Log("4");
+
   ref_difftest_exec = dlsym(handle, "difftest_exec");
   assert(ref_difftest_exec);
-Log("6");
-  Log("Differential testing is turned on.");
-Log("7");
+
   ref_difftest_memcpy_from_dut(ENTRY_START, guest_to_host(ENTRY_START), PMEM_SIZE);
   ref_difftest_setregs(&cpu); 
+
+  difftest_on = true;
+  Log("Differential testing is turned on.");
 }
 
 void difftest_step(uint32_t eip) {
