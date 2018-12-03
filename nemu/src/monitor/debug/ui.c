c989c2e1885e8bfca429255917cb3b1ff03f83d5
tracer-ics2018 <tracer@njuics.org>
2018-12-03 16:38:31 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  16:38:31 up 3 days,  1:51,  0 users,  load average: 3.03, 2.14, 1.95 be6e412e67d234ce827004d45be1ccfdc0c07a67

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -281,17 +281,9 @@ static int cmd_save(char *args) {
     fprintf(fp, "----NEMU STATE SAVEFILE----\n");
     fprintf(fp, "------BEGIN CPU STATE------\n");
     {
-      /* -- GPR+EIP -- */
-		  for (int i = 0; i < 8; ++i) {
-		  	fprintf(fp, "%d\n", reg_l(i));
-		  }
-		  fprintf(fp, "%d\n", cpu.eip);
-      /* -- EFLAGS -- */
-		  fprintf(fp, "%d\n", cpu.eflags32);
-      /* -- IDTR -- */
-      fprintf(fp, "%d %d\n", cpu.IDTR.base, cpu.IDTR.limit);
-      fprintf(fp, "-------END CPU STATE-------\n");
+      fwrite(&cpu, sizeof(CPU_state), 1, fp);
     }
+    fprintf(fp, "-------END CPU STATE-------\n");
     printf("[\033[1;31mSAVE\033[0m] NEMU state save finished.\n");
     fclose(fp);
   }
