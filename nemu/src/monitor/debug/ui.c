a5821074f43925919b0417676f7212a6ddbfab23
tracer-ics2018 <tracer@njuics.org>
2018-12-03 16:32:46 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  16:32:46 up 3 days,  1:46,  0 users,  load average: 2.18, 2.17, 1.93 127a91c82685db62c5e7b65adfa69dd56e5c6291

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -290,6 +290,7 @@ static int cmd_save(char *args) {
     /* -- IDTR -- */
     fprintf(fp, "IDTR base \033[1;33m0x%08x\033[0m limit \033[1;33m%d\033[0m\n", cpu.IDTR.base, cpu.IDTR.limit);
     fprintf(fp, "-------END CPU STATE-------\n");
+    printf("[\033[1;31mSAVE\033[0m] NEMU state save finished.\n");
   }
   return 0;
 }
