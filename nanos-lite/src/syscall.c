d634328357a1e92476311173070fecc69ff544fa
tracer-ics2018 <tracer@njuics.org>
2018-12-03 19:03:42 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  19:03:42 up 3 days,  4:17,  0 users,  load average: 2.12, 2.05, 2.09 4a9cc35b51c523e3f28577858156c4ff81eef4e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -169,7 +169,7 @@ _Context* do_syscall(_Context *c) {
 #ifdef SYS_DEBUG
       Log("SYS_execve(file=%s)", (const char *) a[1]);
 #endif
-      init_proc((const char *) a[1], (char** const) a[2], (char** const) a[3]);
+      init_proc((const char *) a[1], (char* const*) a[2], (char* const*) a[3]);
       break;
 
     default: panic("Unhandled syscall ID = %d, fix in nanos/src/syscall.c", a[0]);
