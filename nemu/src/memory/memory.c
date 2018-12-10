df1bb6987983575f464b8c006d77e1f203fcb51c
tracer-ics2018 <tracer@njuics.org>
2018-12-11 01:54:41 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  01:54:41 up 10 days, 11:08,  0 users,  load average: 2.17, 1.45, 1.11 5f53e7868ac6f198dcdb2ba973ab45272e509558

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -90,8 +90,7 @@ paddr_t page_translate(vaddr_t vaddr, int len) {
 paddr_t do_page_translate(int dir, int page, int offset) {
   printf("in translator!, dir=%d, page=%d, offset=%d\n", dir, page, offset);
   paddr_t dir_entry, pg_entry, paddr;
-  dir_entry = paddr_read(GET_FRAME_ADDR(cpu.CR[3]), 4) + dir;
-  printf("kpdir (CR3) is 0x%08x\n", cpu.CR[3]);
+  dir_entry = paddr_read(GET_FRAME_ADDR(cpu.CR[3]) + dir, 4);
   ASSERT_PRESENT(dir_entry, "DIRECTORY");
   pg_entry  = paddr_read(GET_FRAME_ADDR(dir_entry), 4) + page;
   ASSERT_PRESENT(pg_entry, "PAGE TABLE");
