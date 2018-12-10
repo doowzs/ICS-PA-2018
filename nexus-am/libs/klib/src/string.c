76c21bdab8df8f91d4cd47eed470ad6cc16011bd
tracer-ics2018 <tracer@njuics.org>
2018-12-10 23:20:17 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  23:20:17 up 10 days,  8:33,  0 users,  load average: 1.57, 2.22, 1.85 52d43e05a26de24a98113b9aafaccbf1274e7db6

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -80,7 +80,7 @@ void* memset(void* v, int c, size_t n) {
 
 void* memcpy(void* out, const void* in, size_t n) {
   printf("in memcopy!\n");
-  printf("ADDRESS: %p->%p\n", in, out);
+  printf("COPY ADDRESS: %p->%p\n", in, out);
   int8_t *pout = (int8_t *) out;
   int8_t *pin = (int8_t *) in;
 
