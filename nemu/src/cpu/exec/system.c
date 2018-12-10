635b48f93afc0c2b7e0f8caf60f80087f108c862
tracer-ics2018 <tracer@njuics.org>
2018-12-11 00:00:58 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  00:00:58 up 10 days,  9:14,  0 users,  load average: 2.02, 1.58, 1.52 d6131219f126cdbbc48571d4f70614fd74760292

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -17,6 +17,7 @@ make_EHelper(mov_r2cr) {
   operand_write(id_dest, &id_src->val);
   //printf("moving R No.%d to CR No.%d with value 0x%08x\n", id_src->reg, id_dest->reg, id_src->val);
 
+  printf("CR0 is now 0x%08x\n", cpu.CR[0]);
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 
 #if defined(DIFF_TEST)
