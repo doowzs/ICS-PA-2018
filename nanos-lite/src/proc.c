b5c54d171f2568732952778e5abbde13a8af05ab
tracer-ics2018 <tracer@njuics.org>
2018-12-11 11:26:25 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  11:26:25 up 10 days, 20:39,  0 users,  load average: 1.54, 1.18, 1.15 fb61edfecbdd54e822ead58790f58b74d19f61a1

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -34,8 +34,7 @@ _Context* schedule(_Context *prev) {
   current = &pcb[0];
   //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 #ifdef SYS_DEBUG
-  Log("switching to context at %p, will start at %p", current->cp, current->cp->eip);
+  Log("switching to context at %p, will start at %p and set CR3 to 0x%08x", current->cp, current->cp->eip, current->cp->prot->ptr);
 #endif
-  printf("in schedule, the PTR address of next context is 0x%08x\n", current->cp->prot->ptr);
   return current->cp;
 }
