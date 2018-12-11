fac62f526f9e4d05166ff23e9636a47902a3bf6a
tracer-ics2018 <tracer@njuics.org>
2018-12-12 00:27:24 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  00:27:24 up 11 days,  9:40,  0 users,  load average: 2.05, 2.02, 2.20 e8937147a2522299da90a96c14e4cf8d1ed58730

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,7 +32,7 @@ void init_proc(const char *filename, char* const argv[], char* const envp[]) {
 
 _Context* schedule(_Context *prev) {
   current->cp = prev;
-  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  //
 #ifdef SYS_DEBUG
   Log("switching to context at %p, will start at %p and set CR3 to 0x%08x", current->cp, current->cp->eip, current->cp->prot->ptr);
 #endif
