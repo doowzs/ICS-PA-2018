9e6f5901c9cb5341d79a7f593e93064bac8a1210
tracer-ics2018 <tracer@njuics.org>
2018-12-11 22:51:38 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  22:51:38 up 11 days,  8:05,  0 users,  load average: 2.34, 2.48, 2.24 d92de094ef547a7188ff338e5b032edb785e70e5

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -46,6 +46,8 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
+  if (increment == 0) return brk_old;
+
   brk_ret = brk_old;
   brk_new = brk_old + increment;
   int ret = _syscall_(SYS_brk, (uintptr_t) brk_new, 0, 0);
