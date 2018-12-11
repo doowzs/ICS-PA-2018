873ba262c0ab09f3cc16b0eac69aba7ce4dc2990
tracer-ics2018 <tracer@njuics.org>
2018-12-12 00:02:27 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  00:02:27 up 11 days,  9:15,  0 users,  load average: 2.96, 2.31, 2.16 a6c93459b4b3c2861f7b7533697f2437f8efe

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,13 +26,14 @@ void hello_fun(void *arg) {
 
 void init_proc(const char *filename, char* const argv[], char* const envp[]) {
   Log("special init proc for testing SUCK PA4.2 MMAP!!!");
+
   context_uload(&pcb[0], "/bin/pal");
-  //switch_boot_pcb();
+  context_uload(&pcb[1], "/bin/hello");
 }
 
 _Context* schedule(_Context *prev) {
   current->cp = prev;
-  current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 #ifdef SYS_DEBUG
   Log("switching to context at %p, will start at %p and set CR3 to 0x%08x", current->cp, current->cp->eip, current->cp->prot->ptr);
 #endif
