50714097594f268cdc5622bc8ebc3dd0ccd3760a
tracer-ics2018 <tracer@njuics.org>
2018-12-11 18:03:10 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  18:03:10 up 11 days,  3:16,  0 users,  load average: 1.76, 1.26, 1.31 4b4153a371c08c7c39b196bd3fbc6b5b1fda8c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,15 +10,18 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   void *buf = (void *) DEFAULT_ENTRY;
   size_t sz = fs_filesz(fd);
+
 #ifdef SYS_DEBUG
   Log("loading file %s to %p, fd=%d, sz=%d", filename, buf, fd, sz);
 #endif
+
   int nr_pg = sz / PGSIZE + 1;
   void *pa = new_page(nr_pg); // allocate new pages
   _map(&pcb->as, buf, pa, nr_pg); // map va to pa
-  Log("vaddr %p mapped to %p", buf, pa);
+  pcb->cur_brk = DEFAULT_ENTRY + sz;
+  pcb->max_brk = DEFAULT_ENTRY + sz; // ser brk
+
   fs_read(fd, buf, sz);
-  Log("file %s loaded!", filename);
 
   return (intptr_t) buf;
 }
@@ -50,9 +53,6 @@ void context_uload(PCB *pcb, const char *filename) {
   _Area stack;
   stack.start = pcb->stack;
   stack.end = stack.start + sizeof(pcb->stack);
-
-  pcb->cur_brk = DEFAULT_ENTRY;
-  pcb->max_brk = DEFAULT_ENTRY;
 
   pcb->cp = _ucontext(&pcb->as, stack, stack, (void *)entry, NULL);
 }
