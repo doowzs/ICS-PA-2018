a49a1ccce7513067ab23af08cf65f64ed62332c9
tracer-ics2018 <tracer@njuics.org>
2018-12-11 10:53:14 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  10:53:14 up 10 days, 20:06,  0 users,  load average: 2.68, 1.87, 1.54 6c9925f3f9278ecfdcedc42d629b7dd0ab84d986

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -79,10 +79,10 @@ paddr_t page_translate(vaddr_t vaddr, int len) {
     if (offset + len > PAGE_SIZE) {
       panic("Address exceeds page boundary! dir=%d, page=%d, offset=%d, len=%d", dir, page, offset, len);
     } else {
-      //printf("translate address 0x%08x\n", vaddr);
-      //printf("-> dir=%d, page=%d, offset=%d\n", dir, page, offset);
+      printf("translate address 0x%08x\n", vaddr);
+      printf("-> dir=%d, page=%d, offset=%d\n", dir, page, offset);
       paddr_t paddr = do_page_translate(dir, page, offset);
-      //printf("-> result is 0x%08x\n", paddr);
+      printf("-> result is 0x%08x\n", paddr);
       return paddr;
     }
   } else {
@@ -94,13 +94,13 @@ paddr_t page_translate(vaddr_t vaddr, int len) {
 paddr_t do_page_translate(int dir, int page, int offset) {
   paddr_t PDE, PTE;
   PDE = paddr_read(NEXT_PG(cpu.CR[3], dir), 4);
-  //printf("-> PDE at 0x%08x, is 0x%08x\n", 
-  //    NEXT_PG(cpu.CR[3], dir), PDE);
+  printf("-> PDE at 0x%08x, is 0x%08x\n", 
+      NEXT_PG(cpu.CR[3], dir), PDE);
   ASSERT_PRESENT(PDE, "PDE(level 1)");
 
   PTE = paddr_read(NEXT_PG(PDE, page), 4);
-  //printf("-> PTE at 0x%08x, is 0x%08x\n",
-  //    NEXT_PG(PDE, page), PTE);
+  printf("-> PTE at 0x%08x, is 0x%08x\n",
+      NEXT_PG(PDE, page), PTE);
   ASSERT_PRESENT(PTE, "PTE(level 2)");
 
   return ((PTE >> 12) << 12) + offset;
