7171aaaed206810c462fc9f8a67826b8cf9b6fac
tracer-ics2018 <tracer@njuics.org>
2018-12-11 15:10:39 +0800

    >  compile 171860508 doowzs Linux localhost 4.4.159-15341-g7967a010c95c #1 SMP PREEMPT Sun Nov 25 23:43:14 PST 2018 x86_64 GNU/Linux  15:10:39 up 11 days, 24 min,  0 users,  load average: 2.13, 1.61, 1.72 104f5df31b293178fac7cadfde7a7f7cefd91597

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -83,8 +83,9 @@ uint32_t vaddr_read(vaddr_t vaddr, int len) {
         // 1 + 3
         ret = ((upper & 0xff000000) >> 24) | ((lower & 0x00ffffff) <<  8);
     }
-    printf("read 0x%08x + 0x%08x -> 0x%08x, should be 0x%08x\n", upper, lower, ret, paddr_read(vaddr, 4));
-    return ret >> (8 << (4 - len));
+    ret = ret >> ((4 - len) << 3);
+    printf("read 0x%08x + 0x%08x -> 0x%08x, should be 0x%08x\n", upper, lower, ret, paddr_read(vaddr, len));
+    return ret;
   }
 }
 
