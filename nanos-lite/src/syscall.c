fa8b8b8ce1fa8e31796b46799ce4fd134bce5ff7
tracer-ics2018 <tracer@njuics.org>
2018-12-14 16:57:34 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15346-gc5e629939ae0 #1 SMP PREEMPT Mon Dec 3 22:41:11 PST 2018 x86_64 GNU/Linux  16:57:34 up 1 day,  2:10,  0 users,  load average: 1.87, 1.90, 1.70 e339fa2944243f565fbec44d6df343bc64df5c0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -33,7 +33,7 @@ _Context* do_syscall(_Context *c) {
       Log("SYS_exit(code=%d)", a[1]);
 #endif
       if (a[1] == 0) {
-        c->prot = NULL;
+        _yield();
       } else {
         _halt(a[1]);
       }
