dd474f46288eccf61dd514b9535d2170bcd661ea
tracer-ics2018 <tracer@njuics.org>
2018-12-17 08:36:09 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15346-gc5e629939ae0 #1 SMP PREEMPT Mon Dec 3 22:41:11 PST 2018 x86_64 GNU/Linux  08:36:09 up 2 days, 12:24,  0 users,  load average: 1.84, 2.17, 1.97 326bed4a24f4412eae5f0c667016191d726abc38

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,9 +6,7 @@ _Context* do_syscall(_Context* c);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_IRQ_TIMER:
-#ifdef SYS_DEBUG
       Log("A timer event is triggered.");
-#endif
       return schedule(c, false);
     case _EVENT_YIELD:
 #ifdef SYS_DEBUG
