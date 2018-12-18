70ea72a477919f74e1da0fed74ea2876390bbd0d
tracer-ics2018 <tracer@njuics.org>
2018-12-18 19:32:03 +0800

    >  run 171860508 doowzs Linux localhost 4.4.159-15346-gc5e629939ae0 #1 SMP PREEMPT Mon Dec 3 22:41:11 PST 2018 x86_64 GNU/Linux  19:32:03 up 8 min,  0 users,  load average: 2.72, 1.58, 0.79 e2dc3fe7218d8d7e1a017054596c5fab76d7b10b

--- nexus-am/am/arch/x86-navy/src/devices/timer.c
+++ nexus-am/am/arch/x86-navy/src/devices/timer.c
@@ -2,6 +2,7 @@
 #include <x86.h>
 #include <amdev.h>
 #include <ndl.h>
+#include <stdio.h>
 
 NDL_Event event;
 int cur_time;
@@ -13,6 +14,7 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
       NDL_WaitEvent(&event);
       switch (event.type) {
         case NDL_EVENT_TIMER:
+          printf("time is %d\n", event.data);
           uptime->hi = 0;
           uptime->lo = event.data;
           cur_time = event.data;
